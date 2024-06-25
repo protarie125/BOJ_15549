9ad8469ae5d6a284503791c2018e3a24a977d7f8
protarie125 <protarie@gmail.com>
2024-06-25 20:35:00 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = int64_t;
+using vl = vector<ll>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  constexpr auto x = numeric_limits<int>::min();
+  cout << "int " << x;
+
+  return 0;
+}
